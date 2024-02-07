af0ebb06c3e809a61b715e1128b4ebae570009b2
thesuperfalcon <tintinfalk01@gmail.com>
2024-02-07 11:38:25 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeaterData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
